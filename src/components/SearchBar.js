f4fa421cbac6780a71df8da970a80d8d9fd70a42
Andrew Rubenstein <andjruben@gmail.com>
2022-08-30 21:32:22 -0400

    Add button to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './SearchBar.css';
 import TextField from "@mui/material/TextField";
+import { Button } from '@mui/material';
 
 export const SearchBar = () => {
   const [input, setInput] = useState("");
@@ -12,14 +13,20 @@ export const SearchBar = () => {
 
   return (
     <div className="searchbar">
-      <TextField
-        className="input"
-        id="outlined-basic"
-        onChange={handleInput}
-        variant="outlined"
-        fullWidth
-        label="Search"
-      />
+      <div className="bar">
+        <TextField
+          className="input"
+          id="outlined-basic"
+          onChange={handleInput}
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+
+      <div className="searchbutton">
+        <Button variant="contained" className="button">Search</Button>
+      </div>
     </div>
   );
 }
