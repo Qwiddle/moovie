458acbe595fdb7fb8d826dc89bb60404c1b21af3
Andrew <andjruben@gmail.com>
2022-09-03 14:25:38 -0400

    Add favorite icon to ResultList items

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,16 +1,25 @@
 import './ResultList.css';
+import { Button, IconButton } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { red } from '@mui/material/colors';
+
 
 export const ResultList = ({data}) => {
   return (
     data ?
     <div className="results">
-      {data.map((movie) => (
-        <div key={movie.id}>
-          <h1>{movie.name}</h1>
-          <p>{movie.overview}</p>
-          <strong>👀 {movie.popularity}</strong>{' '}
-          <strong>✨ {movie.vote_average}</strong>{' '}
-          <strong>🍴 {movie.vote_count}</strong>
+      {data.map((result) => (
+        <div key={result.id} className="item">
+          <div className="header">
+            <IconButton aria-label="favorite" className="favorite">
+              <FavoriteIcon sx={{color: red[800]}} />
+            </IconButton>
+            <h1>{result.name}</h1>
+          </div>
+          <p>{result.overview}</p>
+          <strong>👀 {result.popularity}</strong>{' '}
+          <strong>✨ {result.vote_average}</strong>{' '}
+          <strong>🍴 {result.vote_count}</strong>
         </div>
       ))}
     </div> : ''
