14bec355edce02a5d9ec851826a10ed0d22d6447
Andrew <andjruben@gmail.com>
2022-09-05 01:15:12 -0400

    Tweak MovieComponent to use ContentItem

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
-import { Image } from "mui-image";
-import {
-  Divider,
-  Button,
-  Typography 
-} from '@mui/material';
-import { Favorite } from "./Favorite";
-import './Movie.css';
+import { ContentItem } from "./ContentItem";
 
 export const Movie = () => {
-  let params = useParams();
+  const params = useParams();
 
   const { isLoading, data } = useQuery(['movie'], async () => {
     const res = await fetch(`https://api.themoviedb.org/3/movie/${params.cid}?api_key=${process.env.REACT_APP_API_TMDB}`)
@@ -22,43 +15,8 @@ export const Movie = () => {
   });
 
   return (
-    isLoading ?
-      'Loading...' :
-      <div 
-        type="movie" 
-        className="movie-item">
-        <Image 
-          src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
-          fit="cover"
-          height="40vh"
-          duration={0}
-          className="movie-media"
-        />
-        <div className="movie-content">
-          <div className="movie-header">
-            <h1>{data.title}</h1>
-            <Favorite cid={Number(params.cid)} type="movie"/>
-          </div>
-          <p>{data.overview}</p>
-          <strong>👀 {data.popularity}</strong>{' '}
-          <strong>✨ {data.vote_average}</strong>{' '}
-          <strong>🔄 {data.vote_count}</strong>
-          <Divider className='movie-divider' light>
-            <Typography variant={"caption"}>
-              options
-            </Typography>
-          </Divider>
-          <Button 
-            variant="contained" 
-            sx={{ mt: 0.5 }}>
-            Share
-          </Button>
-          <Button 
-            variant="contained" 
-            sx={{ mt: 0.5 }}>
-            Recommend similar
-          </Button>
-        </div>
-      </div>
+    isLoading ? 
+    'Loading...' :
+    <ContentItem cid={params.cid} data={data}/>
   )
 }
