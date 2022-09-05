99a1aea15e1d93c9caa68a93de175c84bc456458
Andrew <andjruben@gmail.com>
2022-09-05 01:15:25 -0400

    Add TV component

---
+++ src/components/TV.js
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { ContentItem } from "./ContentItem";
+import './ContentItem.css';
+
+export const TV = () => {
+  const params = useParams();
+
+  const { isLoading, data } = useQuery(['movie'], async () => {
+    const res = await fetch(`https://api.themoviedb.org/3/tv/${params.cid}?api_key=${process.env.REACT_APP_API_TMDB}`)
+    const json = await res.json();
+
+    return json;
+  }, { 
+    enabled: !!params.cid 
+  });
+
+  return (
+    isLoading ? 
+    'Loading...' :
+    <ContentItem cid={params.cid} data={data} />
+  )
+}
